b39628f7d4427d7a39cedac6b73b65faf87c15ba
nithyamajeti06 <majetinithya@gmail.com>
2026-02-13 18:08:03 -0500

    New Turn Class

---
+++ Task4/Turn.java
@@ -0,0 +1,25 @@
+public class Turn {
+
+    private Dice dice;
+    private Production production;
+    private Board board;
+
+    public Turn(Dice dice, Production production, Board board) {
+        this.dice = dice;
+        this.production = production;
+        this.board = board;
+    }
+
+    public String execute(Player player, int roundNumber) {
+
+        int roll = dice.roll();
+        production.generateResources(roll);
+
+        String actionResult = player.takeRandomAction(board);
+
+        return "[" + roundNumber + "] Player "
+                + player.getPlayerID()
+                + " rolled " + roll
+                + " -> " + actionResult;
+    }
+}
