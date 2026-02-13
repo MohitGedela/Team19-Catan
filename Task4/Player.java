3b3bf33a8e9c2557fe13ec785d12863bd24543d4
nithyamajeti06 <majetinithya@gmail.com>
2026-02-13 04:29:18 -0500

    Creating Player Class

---
+++ Task4/Player.java
@@ -0,0 +1,63 @@
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Player {
+    private int playerID;
+    private int victoryPoints;
+    private List<City> playerCities;
+    private List<Settlement> playerSettlements;
+    private Map<ResourceType, Integer> playerResources;
+    
+    public Player(int playerNum, int playerVP, List<City> cities, List<Settlement> settlements, Map<ResourceType, Integer> resources) {
+        playerID = playerNum;
+        victoryPoints = playerVP;
+        playerCities = cities;
+        playerSettlements = settlements;
+        playerResources = resources;
+    }
+
+    public void addResource(ResourceType resource, int quantity) {
+        if (playerResources.containsKey(resource)) {
+            playerResources.put(resource, playerResources.get(resource) + quantity);
+        }
+        System.out.println("Succesfully added");
+    }
+
+    public boolean removeResource(ResourceType resource, int quantity) {
+        if (quantity > playerResources.get(resource) && playerResources.containsKey(resource)) {
+            System.out.println("Unsuccesful not enough resources");
+            return false;
+        }
+
+        if (playerResources.containsKey(resource)) {
+            playerResources.put(resource, playerResources.get(resource) - quantity);
+        }
+        return true;
+    }
+
+    public boolean checkResource(ResourceType resource, int quantity) {
+        if (playerResources.containsKey(resource) && playerResources.get(resource) == quantity) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getVictoryPoints() {
+        return victoryPoints;
+    }
+
+    public void buildSettlement(Intersection buildIntersection) {
+
+    }
+
+    public void buildCity(Intersection buildIntersection) {
+
+    }
+
+    public void buildRoad(Edge buildEdge) {
+
+    }
+}
